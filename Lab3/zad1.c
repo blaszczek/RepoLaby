21b77d03d52cea098c136087c39fac91e1d7b8d7
blaszczek <blaszczek@outlook.com>
2016-03-31 20:33:48 +0200

    Update zad1.c

--- Lab3/zad1.c
+++ Lab3/zad1.c
@@ -1,31 +1,16 @@
-#include<stdio.h>
-#include<float.h>
-#include<limits.h>
-
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
 int main()
 {
-  FILE *fp;
-    if ((fp=fopen("zapisanie.txt", "w"))==NULL)
-	{
-       printf ("blad\n");
-       return;
-    }
-
-
-
-    fprintf(fp,"|typ        |                  min|                  max|    ziarno| prec\t\t\t|we/wy|\n");
-    fprintf(fp,"|-----------------------------------------------------------------------------------------------|-----|\n");
-
-    fprintf(fp,"|short      | %20i| %20i|          |     \t\t\t| i   |\n",SHRT_MIN, SHRT_MAX);
-    fprintf(fp,"|int        | %20d| %20d|          |     \t\t\t| d   |\n",INT_MIN, INT_MAX);
-    fprintf(fp,"|long       | %20li| %20li|          |     \t\t\t| li  |\n",LONG_MIN,LONG_MAX);
-    fprintf(fp,"|long long  | %20lli| %20lli|          |     \t\t\t| lli |\n",LONG_LONG_MIN,LONG_LONG_MAX);
-    fprintf(fp,"|float      |              3.4E-38|              3.4E+38|       %d  |   %d \t\t| f   |\n",FLT_EPSILON,FLT_DIG);
-    fprintf(fp,"|double     |             1.7E-308|             1.7E+308|       %d  |  %d \t\t| lf  |\n",DBL_EPSILON,DBL_EPSILON);
-    fprintf(fp,"|long double|             1.7E-308|             1.7E+308|       %d  |  %d \t\t| Lf  |\n",LDBL_EPSILON,LDBL_DIG);
-    fprintf(fp,"|-----------------------------------------------------------------------------------------------------|\n");
-    fclose (fp);
-
-
-return 0;
+  printf("|%11s|%8s|%21s|%21s|%13s|%10s|\n", "TYP", "ROZMIAR", "MIN", "MAX", "ZIARNO", "PRECYZJA");
+  printf("|-----------|--------|---------------------|---------------------|-------------|----------|\n");
+  printf("|%11s| %7li| %20i| %20i|%13c|%10c|\n", "short", sizeof(short), (short)SHRT_MIN, (short)SHRT_MAX, ' ', ' ');
+  printf("|%11s| %7li| %20d| %20d|%13c|%10c|\n", "int", sizeof(int), (int)INT_MIN, (int)INT_MAX, ' ', ' ');
+  printf("|%11s| %7li| %20li| %20li|%13c|%10c|\n", "long", sizeof(long), (long)LONG_MIN, (long)LONG_MAX, ' ', ' ');
+  printf("|%11s| %7li| %20lli| %20lli|%13c|%10c|\n", "long long", sizeof(long long), (long long)LLONG_MIN, (long long)LLONG_MAX, ' ', ' ');
+  printf("|%11s| %7li| %20e| %20e| %e| %9d|\n", "float", sizeof(float), (float)FLT_MIN, (float)FLT_MAX, FLT_EPSILON, (int)FLT_DIG);
+  printf("|%11s| %7li| %20le| %20le| %e| %9d|\n", "double", sizeof(double), (double)DBL_MIN, (double)DBL_MAX, DBL_EPSILON, (int)DBL_DIG);
+  printf("|%11s| %7li| %20Le| %20Le| %Le| %9d|\n", "long double", sizeof(long double), (long double)LDBL_MIN, (long double)LDBL_MAX, LDBL_EPSILON, (int)LDBL_DIG);
+  return 0;
 }

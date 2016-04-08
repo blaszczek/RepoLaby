291cb0eec7b84fbfb8026a7da406f7fdbc9ebc2e
blaszczek <blaszczek@outlook.com>
2016-04-08 10:30:21 +0200

    Update zad2.c

--- Lab5/zad2.c
+++ Lab5/zad2.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
+#include<stdlib.h>
 
 #define IMIE_MAX 10
 #define NAZW_MAX 15
@@ -17,7 +18,8 @@ osoba spis[IL_OSOB];
 //=======================================================
 
 void  utworz_spis(void) {
-  FILE* baza =fopen("/home/pracinf/stefan/public_html/Dydaktyka/JezProg/Slajdy/Labs05/baza_danych","r");
+  FILE* baza =
+    fopen("mBaza","r");
   if (baza == NULL) printf("\n ZLE\n\n");
   for (int i=0; i<IL_OSOB; i++) {
     fscanf(baza, "%s", spis[i].imie);
@@ -29,80 +31,81 @@ void  utworz_spis(void) {
 
 //=======================================================
 
-void  sortuj_spis(void) {
-  /* sortuje  spis  alfabetycznie wg nazwisk,
-     a w przypadku rownych nazwisk wg imion
-  */
-  int i, j;
-  char pom[NAZW_MAX+1];
-  for(i=1;i<=IL_OSOB;i++)
+int compare(const void *s1, const void *s2, void *arg)
   {
-    for(j=i;j<IL_OSOB;j++)
-    {
-      if(strcmp(spis[j-1].nazwisko, spis[j].nazwisko)>0)
-      {
-        strcpy(pom, spis[j-1].nazwisko);
-        strcpy(spis[j-1].nazwisko, spis[j].nazwisko);
-        strcpy(spis[j].nazwisko, pom);
-      }
-      else if(strcmp(spis[j-1].nazwisko,spis[j].nazwisko)==0)
-      {
-        strcpy(pom,spis[j-1].imie);
-        strcpy(spis[j-1].imie, spis[j].imie);
-        strcpy(spis[j].imie, pom);
-      }
+    osoba *o1 = (osoba *)s1;
+    osoba *o2 = (osoba *)s2;
+    int *n = (int *)arg;
+    switch ( *n ) {
+      case 1:
+        return strcmp(o1 -> nazwisko, o2 -> nazwisko);
+        break;
+      case 2:
+        return strcmp(o1 -> imie, o2 -> imie);
+        break;
+      case 3:
+        if ( o1 -> pensja > o2 -> pensja) {
+          return -1;
+        }
+        break;
+      default:
+      break;
     }
-  }
-  FILE* baza2=fopen("posortowane.txt","w");
-  for(i=0;i<IL_OSOB;i++)
-  {
-    fprintf(baza2, "%s	", spis[i].imie);
-    fprintf(baza2, "%s	", spis[i].nazwisko);
-    fprintf(baza2, "%i\n", spis[i].pensja);
-  }
-  fclose(baza2);
 }
+// powyzsza funkcja generuje ostrzerzenie -Wreturn-type
 //=======================================================
 
-int  znajdz_nazwisko (
-  char na[NAZW_MAX+1],
-  char im[IMIE_MAX+1], int *p
-) {
-  /* do danego nazwiska  na  znajduje w spisie
-     imie  im  oraz pensje  p
-     jesli znajdzie, to zwraca 1, jesli nie, to 0
-  */
-       int i=0;
-       while(i<IL_OSOB && strcmp(na, spis[i].nazwisko) != 0)
-         i++;
-       if(i == IL_OSOB) return 0;
-       else if(strcmp(na, spis[i].nazwisko)==0)
-         strcpy(im, spis[i].imie);
-       *p = spis[i].pensja;
-       return 1;
+
+void  sortuj_spis() {
+  int opcja;
+  printf("Jak chcesz posortować spis? : \n");
+  scanf("%d",&opcja);
+  qsort_r ( spis, IL_OSOB, sizeof(osoba), compare, &opcja);
 }
 
 //=======================================================
+void spis_do_pliku() {
 
-int  znajdz_imie (
-  char im[NAZW_MAX+1],
-  char na[IMIE_MAX+1], int *p
-) {
-  /* do danego imienia  im  znajduje w spisie
-     nazwisko  na  oraz pensje  p
-     jesli znajdzie, to zwraca 1, jesli nie, to 0
-  */
-  int i;
-  for(i=0; i<IL_OSOB; i++)
-  {
-    if(strcmp(spis[i].imie,im) == 0){
-      strcpy(na,spis[i].nazwisko);
-      *p = spis[i].pensja;
-      return 1;
-    }
+
+  FILE *fp = fopen("posortowany", "w");
+
+  if( !fp ) {
+    printf("Błąd odczytu pliku");
   }
+    for( int i = 0; i < IL_OSOB; ++i) {
+      if(spis[i].pensja)
+        fprintf(fp,"%s %s %i\n",spis[i].imie, spis[i].nazwisko, spis[i].pensja);
+      else continue;
+    }
+  fclose(fp);
+}
+//=======================================================
+int  znajdz_nazwisko (char *na, char *im, int *p)
+{
 
-return 0;
+	for(int i = 0; i < IL_OSOB; ++i){
+		if(strcmp( spis[i].nazwisko, na) == 0){
+		strcpy( im, spis[i].imie );
+		*p = spis[i].pensja;
+		return 1;
+		}
+	}
+	return 0;
+}
+
+//=======================================================
+//połaczyć znajdz nazwisko i znajdz imie
+int  znajdz_imie (char *im, char *na, int *p)
+{
+
+	for( int i = 0; i < IL_OSOB; ++i ){
+		if( strcmp( spis[i].imie, im) == 0 ){
+		strcpy( na, spis[i].nazwisko );
+		*p = spis[i].pensja;
+		return 1;
+		}
+	}
+	return 0;
 }
 
 //=======================================================
@@ -111,7 +114,7 @@ int main () {
   char odpowiedz, im[NAZW_MAX+1], na[IMIE_MAX+1];
   int p;
 
-  utworz_spis(); sortuj_spis();
+  utworz_spis(); sortuj_spis();spis_do_pliku();
 
   do {
     printf(
@@ -133,7 +136,7 @@ int main () {
       scanf("%s", na);
       if (znajdz_nazwisko(na, im, &p))
 	printf(" IMIE: %s, NAZWISKO: %s, PENSJA: %i\n", im, na, p);
-      else  printf(" nie ma nazwiska %s\n", na);
+      else  printf(" nie ma nazwiska %s\n", im);
       break;
     case 'q' : break;
     default :
@@ -142,5 +145,4 @@ int main () {
   }  while (tolower(odpowiedz) != 'q');
 
   printf("\n DZIEKUJE.\n\n");
-  return 0;
 }
